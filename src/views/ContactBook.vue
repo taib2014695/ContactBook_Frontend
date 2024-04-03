c1434edecabf5db115a591b3f0b5603859bf1373
Huu Tai <taib2014695@student.ctu.edu.vn>
2024-04-03 21:59:21 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -8,13 +8,10 @@
                 Danh bạ
                 <i class="fas fa-address-book"></i>
             </h4>
-            <ContactList 
-                v-if="filteredContactsCount > 0" 
-                :contacts="filteredContacts"
-                v-model:activeIndex="activeIndex" 
-            />
+            <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts"
+                v-model:activeIndex="activeIndex" />
             <p v-else>Không có liên hệ nào.</p>
-            
+
             <div class="mt-3 row justify-content-around align-items-center">
                 <button class="btn btn-sm btn-primary" @click="refreshList()">
                     <i class="fas fa-redo"></i> Làm mới
@@ -24,9 +21,7 @@
                     <i class="fas fa-plus"></i> Thêm mới
                 </button>
 
-                <button 
-                    class="btn btn-sm btn-danger" 
-                    @click="removeAllContacts">
+                <button class="btn btn-sm btn-danger" @click="removeAllContacts">
                     <i class="fas fa-trash"></i> Xóa tất cả
                 </button>
             </div>
@@ -38,6 +33,14 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link 
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                    }">
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
